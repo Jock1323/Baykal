7e94a910beafd3a87b5b05bfc2969d281ad751fe
Jock1323 <mirsidiq2002@gmail.com>
2022-02-12 12:40:26 +0500

    add btnType logic to js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const toggleEnd=document.querySelector('.toggle__end');
 const toggleStart=document.querySelectorAll('.toggle__start');
 const nav=document.querySelector('.footer__nav');
+const btnType=document.querySelector('.footer__flexed--1 .type');
 toggleEnd.addEventListener('click',()=>{
     nav.style.left='-100%';
 });
@@ -9,5 +10,10 @@ for(let i=0;i<toggleStart.length;i++){
        nav.style.left='0';
     });
 }
+btnType.addEventListener('click',()=>{
+  
+    nav.style.left='-100%';
+})
+
 
 
