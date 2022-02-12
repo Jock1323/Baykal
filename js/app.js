5f44185b73cb9c15eb19cf4d565a688436bbca47
Jock1323 <mirsidiq2002@gmail.com>
2022-02-12 13:22:27 +0500

    add some changes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const toggleEnd=document.querySelector('.toggle__end');
 const toggleStart=document.querySelectorAll('.toggle__start');
 const nav=document.querySelector('.footer__nav');
 const btnType=document.querySelector('.footer__flexed--1 .type');
+
+// Menu bar
 toggleEnd.addEventListener('click',()=>{
     nav.style.left='-100%';
 });
@@ -10,6 +12,8 @@ for(let i=0;i<toggleStart.length;i++){
        nav.style.left='0';
     });
 }
+
+// Mobile menu button
 btnType.addEventListener('click',()=>{
   
     nav.style.left='-100%';
